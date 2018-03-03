523573e6072b784e860a16a5a3abe51d7ae2a9be
Jefferson de Souza Gonçalves <jeffsouza@gmail.com>
2018-03-03 21:22:37 +0000

    Adding update and destroy actions to the users controller with tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,69 +1,118 @@
 require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
-    let!(:user) { create(:user) }
-    let(:user_id) { user.id }
+  let!(:user) { create(:user) }
+  let(:user_id) { user.id }
 
-    before { host! 'api.task-manager.dev' }
+  before { host! 'api.task-manager.dev' }
 
-    describe 'GET /users/:id' do
-        before do
-            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
-            get "/users/#{user_id}", params: {}, headers: headers
-        end
+  describe 'GET /users/:id' do
+      before do
+          headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+          get "/users/#{user_id}", params: {}, headers: headers
+      end
 
-        context 'when the user exists' do
-            it 'returns the user' do
-                user_response = JSON.parse(response.body)
-                expect(user_response['id']).to eq(user_id)
-            end
+      context 'when the user exists' do
+          it 'returns the user' do
+              user_response = JSON.parse(response.body)
+              expect(user_response['id']).to eq(user_id)
+          end
 
-            it 'returns status code 200' do
-                expect(response).to have_http_status(200)
-            end
-        end
+          it 'returns status code 200' do
+              expect(response).to have_http_status(200)
+          end
+      end
 
-        context 'when the user does not exists' do
-            let(:user_id) { 1000 }
+      context 'when the user does not exists' do
+          let(:user_id) { 1000 }
 
-            it 'returns status code 404' do
-                expect(response).to have_http_status(404)
-            end
-        end
+          it 'returns status code 404' do
+              expect(response).to have_http_status(404)
+          end
+      end
 
+  end
+
+  describe 'POST /users' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      post '/users', params: { user: user_params }, headers: headers
     end
 
-    describe 'POST /users' do
-        before do
-            headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
-            post '/users', params: { user: user_params }, headers: headers
-        end
+    context 'when the request params are valid' do
+      let(:user_params) { attributes_for(:user) }
 
-        context 'when the request params are valid' do
-            let(:user_params) { attributes_for(:user) }
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
 
-            it 'returns status code 201' do
-                expect(response).to have_http_status(201)
-            end
-
-            it 'returns json data for the created user' do 
-                user_response = JSON.parse(response.body, symbolize_names: true)
-                expect(user_response[:email]).to eq(user_params[:email])
-            end 
-        end
-
-        context 'when the request params are invalid' do
-            let(:user_params) { attributes_for(:user, email: 'invalid_email@') }
-
-            it 'returns status code 422' do
-                expect(response).to have_http_status(422)
-            end
-
-            it 'returns the json data for the errors' do
-                user_response = JSON.parse(response.body, symbolize_names: true)
-                expect(user_response).to have_key(:errors)
-            end
-        end
+      it 'returns json data for the created user' do 
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email])
+      end 
     end
+
+    context 'when the request params are invalid' do
+      let(:user_params) { attributes_for(:user, email: 'invalid_email@') }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns the json data for the errors' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+    end
+  end
+
+  describe 'PUT /users/:id' do
+    before do
+      headers = { 'Accept': 'application/vnd.taskmanager.v1' }
+      put "/users/#{user_id}", params: {user: user_params}, headers: headers
+    end
+
+    context 'when the request params are valid' do
+      let(:user_params) { { email: 'new_email@taskmanager.com' } }
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the json data for the updated user' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq(user_params[:email])
+      end
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) { { email: 'email_invalid@' } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns the json data for the errors' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+    end
+  end
+
+  describe 'DELETE /users/:id' do
+    before do
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
+      delete "/users/#{user_id}", params: {}, headers: headers
+    end
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+
+    it 'removes the user from database' do
+      expect( User.find_by(id: user.id) ).to be_nil
+    end
+
+  end
 
 end
