749b6bdfcad644293e8297f3a0aac210a133b34f
Jefferson de Souza Gonçalves <jeffsouza@gmail.com>
2018-03-04 23:21:57 +0000

    Adding auth token and #info to user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     password '123456'
     password_confirmation '123456'
+    auth_token { Digest::SHA1.hexdigest([Time.now, rand].join) }
   end
 end
